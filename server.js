90de284cfe7750ab13954875ca43b57014c538de
AadityaNayak <aadityanayak7@gmail.com>
2023-07-11 02:13:33 +0530

    Committing code to github

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./route")
-const dotenv = require(dotenv);
-dotenv.config();
+
 // Importing connection
 require("./db/config");
 
